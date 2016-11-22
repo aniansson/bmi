a820879cc0406aefe2cc9192ebe60f779b611f96
blondabambi <ebba@aniansson.se>
2016-11-22 13:36:01 +0100

    step 1 & 2 done

---
+++ spec/bmi_calculator_spec.js
@@ -0,0 +1,20 @@
+describe("BMICalculator", function() {
+  var calculator;
+  var person;
+
+  beforeEach(function() {
+    person = new Person({weight: 90, height: 186});
+    calculator = new BMICalculator();
+  });
+
+  it("calculates BMI for a person using the metric method", function() {
+    calculator.metric_bmi(person);
+    expect(person.bmiValue).toEqual(26.01);
+  });
+
+  it('should give BMI message', function(){
+    calculator.metric_bmi(person);
+    expect(person.bmiMessage).toEqual('Overweight');
+  });
+
+});
