5f1d8585d64e4b2a8a7aaa75e7128fbddd28f68c
blondabambi <ebba@aniansson.se>
2016-11-23 15:32:03 +0100

    redo so both functions work depending on choosen option

--- src/person.js
+++ src/person.js
@@ -2,14 +2,13 @@ function Person(attr) {
   this.weight = attr.weight;
   this.height = attr.height;
 
-  Person.prototype.calculate_bmi_metric = function() {
-     calculator = new BMICalculator();
-     calculator.metric_bmi(this);
-  };
 
-  Person.prototype.calculate_bmi_imperial =
-  function() {
-      calculator = new BMICalculator();
-      calculator.imperial_bmi(this);
+  Person.prototype.calculate_bmi = function() {
+     calculator = new BMICalculator();
+
+     if('option is Metric') {
+       calculator.metric_bmi(this);
+     }
+     calculator.imperial_bmi(this);
   };
 };
