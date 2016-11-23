01a941ed96316b8761671161e90bc21e28525456
blondabambi <ebba@aniansson.se>
2016-11-23 14:54:39 +0100

    add function for imperial option in ui file

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -8,12 +8,31 @@ describe('BMI_UI - index.html', function() {
       $('#height').val('186');
       $('#calculate').trigger('click');
     });
-  
+
     it('displays BMI Value', function() {
       expect($('#display_value').text()).toBe('Your BMI is 26.01');
     });
 
     it('displays BMI Message', function () {
+      expect($('#display_message').text()).toBe('and you are Overweight');
+    });
+  });
+
+  describe('calculate BMI with imperial option', function() {
+    beforeEach(function() {
+      jasmine.getFixtures().fixturesPath = '.';
+      loadFixtures('index.html');
+      $.holdReady(false);
+      $('#weight').val('198');
+      $('#height').val('73');
+      $('#calculate').trigger('click');
+    });
+
+    it('displays BMI Value', function() {
+      expect($('#display_value').text()).toBe('Your BMI is 26.01');
+    });
+
+    it('displays BMI Message', function() {
       expect($('#display_message').text()).toBe('and you are Overweight');
     });
   });
